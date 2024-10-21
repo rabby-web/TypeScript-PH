ef581fa696240956d2e351179bc9b9e39e20f790
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-21 18:29:43 +0600

    union and intersection

---
+++ M-1/module1/src/1.10.ts
@@ -0,0 +1,36 @@
+{
+  // Union and Intersection Types
+
+  // Union
+  type FDev = "fakeDev" | "jrDev";
+  const newDev: FDev = "jrDev";
+
+  type User = {
+    name: string;
+    email?: string;
+    gender: "male" | "female";
+    bloodGroup: "O+" | "A+" | "AB+";
+  };
+  const user1: User = {
+    name: "persian",
+    gender: "male",
+    bloodGroup: "O+",
+  };
+
+  //   intersection
+  type FrontendDeveloper = {
+    skills: string[];
+    designation1: "Frontend Developer";
+  };
+  type BackendDeveloper = {
+    skills: string[];
+    designation2: "Backend Developer";
+  };
+  type FullstackDeveloper = FrontendDeveloper & BackendDeveloper;
+
+  const fullstackDeveloper: FrontendDeveloper = {
+    skills: ["HTML"],
+    designation1: "Frontend Developer",
+    // designation2: "Backend Developer",
+  };
+}
