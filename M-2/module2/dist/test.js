3274ed886ff2111ab180c239915e45fd06bd291f
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-22 10:41:51 +0600

    type assertion

--- M-2/module2/dist/test.js
+++ M-2/module2/dist/test.js
@@ -1,4 +1,19 @@
 "use strict";
 {
-    console.log("object");
+    // type assertion
+    let anything;
+    anything = "Next Level Wev Development";
+    anything = 200;
+    //   (anything as number)
+    const kgToGm = (value) => {
+        if (typeof value === "string") {
+            const convertedValue = parseFloat(value) * 1000;
+            return convertedValue;
+        }
+        if (typeof value === "number") {
+            return value * 1000;
+        }
+    };
+    const result1 = kgToGm(1000);
+    console.log(result1);
 }
--- M-2/module2/src/test.ts
+++ M-2/module2/src/test.ts
@@ -1,3 +1,21 @@
 {
-  console.log("object");
+  // type assertion
+  let anything: any;
+  anything = "Next Level Wev Development";
+  anything = 200;
+  //   (anything as number)
+
+  const kgToGm = (value: string | number): string | number | undefined => {
+    if (typeof value === "string") {
+      const convertedValue = parseFloat(value) * 1000;
+      return convertedValue;
+    }
+    if (typeof value === "number") {
+      return value * 1000;
+    }
+  };
+  const result1 = kgToGm(1000) as number;
+  console.log(result1);
+  const result2 = kgToGm("1000") as string;
+  console.log(result2);
 }
