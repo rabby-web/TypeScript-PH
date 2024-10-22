9be169e439062fbd3f1613c650c1e5cf8ac6f2c1
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-22 14:16:49 +0600

    function with generic and tuple

---
+++ M-2/module2/src/2.5.ts
@@ -0,0 +1,29 @@
+{
+  // function with generic
+  const createArray = (param: string): string[] => {
+    return [param];
+  };
+
+  const createArrayWithGeneric = <T>(param: T): T[] => {
+    return [param];
+  };
+
+  const res1 = createArray("Bangladesh");
+  const resGeneric = createArrayWithGeneric<string>("Bangladesh");
+
+  type User = { id: number; name: string };
+  const resGenericObj = createArrayWithGeneric<User>({
+    id: 222,
+    name: "Mr. Z",
+  });
+
+  // tuple
+  const createArrayWithTuple = <T, Q>(param1: T, param2: Q): [T, Q] => {
+    return [param1, param2];
+  };
+
+  const res10 = createArrayWithTuple("Bangladesh", 222);
+  const resGeneric10 = createArrayWithTuple<string, string>("USA", "UK");
+
+  //
+}
