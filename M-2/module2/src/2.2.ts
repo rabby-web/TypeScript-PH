3806421162014f928ed4564564941bacc203bdbf
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-22 11:26:05 +0600

    object function type alias

--- M-2/module2/src/2.2.ts
+++ M-2/module2/src/2.2.ts
@@ -21,4 +21,15 @@
   };
 
   // js => object, array => object function => object
+  type Roll1 = number[];
+  interface Roll2 {
+    [index: number]: number;
+  }
+  const rollNumber: Roll2 = [1, 2, 3];
+
+  type Add1 = (num1: number, num2: number) => number;
+  interface Add2 {
+    (num1: number, num2: number): number;
+  }
+  const add: Add2 = (num1, num2) => num1 + num2;
 }
