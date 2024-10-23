da4c9125a4ad3e7ac637807db0fb0f5393cd9e2a
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-23 17:31:01 +0600

    class opp add

--- M-3/module3/src/test.ts
+++ M-3/module3/src/test.ts
@@ -1,3 +1,18 @@
 {
-  console.log("object");
+  // class
+  class Animal {
+    constructor(
+      public name: string,
+      public species: string,
+      public sound: string
+    ) {}
+    makeSound() {
+      console.log(`The ${this.name} says ${this.sound}`);
+    }
+  }
+
+  const dog = new Animal("Dog", "dog", "Ghew");
+  const cat = new Animal("Cat", "cat", "Mew");
+
+  //
 }
