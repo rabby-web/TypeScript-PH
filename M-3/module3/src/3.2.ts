a593b499761c122b5c1a937ff9e4f11f83a6672e
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-24 17:48:05 +0600

    oop in inheritance

---
+++ M-3/module3/src/3.2.ts
@@ -0,0 +1,46 @@
+{
+  // oop - inheritance
+  //  parent
+  class Parent {
+    name: string;
+    age: number;
+    address: string;
+    constructor(name: string, age: number, address: string) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+    }
+    getSleep(numOfHours: number) {
+      console.log(`${this.name} with sleep for ${numOfHours}`);
+    }
+  }
+
+  //  student
+  class Student extends Parent {
+    constructor(name: string, age: number, address: string) {
+      super(name, age, address);
+    }
+  }
+  const student = new Student("Mr. student", 20, "Uganda");
+
+  //  teacher
+  class Teacher extends Parent {
+    designation: string;
+    constructor(
+      name: string,
+      age: number,
+      address: string,
+      designation: string
+    ) {
+      super(name, age, address);
+      this.designation = designation;
+    }
+
+    takeClass(numOfClass: number) {
+      console.log(`${this.name} will take ${numOfClass}`);
+    }
+  }
+  const teacher = new Teacher("Mr. teacher", 20, "Uganda", "Prof");
+
+  //
+}
