3172dded9d4dda531c9c38af59b1fbe295a4cb95
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-25 07:31:42 +0600

    abstractions

---
+++ M-3/module3/src/3.9.ts
@@ -0,0 +1,50 @@
+{
+    // abstractions => 1. interface 2. abstract
+    // interface-----------
+    // idea
+    interface Vehicle1 {
+      startEngine(): void;
+      stopEngine(): void;
+      move(): void;
+    }
+    // real implement
+    class Car1 implements Vehicle1 {
+      startEngine(): void {
+        console.log(`I am start engine`);
+      }
+      stopEngine(): void {
+        console.log(`I am stop engine`);
+      }
+      move(): void {
+        console.log(`I am moving engine`);
+      }
+      test() {
+        console.log(`I am tests engine`);
+      }
+    }
+    // abstract------------------
+  
+    //   idea
+    abstract class Car2 {
+      abstract startEngine(): void;
+      abstract stopEngine(): void;
+      abstract move(): void;
+      test() {
+        console.log(`I am tests engine`);
+      }
+    }
+  
+    //   class Toyota extends Car2 {
+    //     startEngine(): void {
+    //       console.log(`I am start engine`);
+    //     }
+    //     stopEngine(): void {
+    //       console.log(`I am start engine`);
+    //     }
+    //     moveEngine(): void {
+    //       console.log(`I am start engine`);
+    //     }
+    //   }
+  
+    //
+  }
