4ddf3fc53b7949d99d6ff9f36ab668934d841575
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-30 20:43:39 +0600

    practice basic type of ts

---
+++ practice-m1/m1/src/1.4.ts
@@ -0,0 +1,23 @@
+// basic ts
+// string
+let myName: string = "My Name is Zulkar Naeem Rabby";
+// number
+let myNumber: number = 10;
+// boolean
+let isStudent: boolean = true;
+// undefined
+let isBadReport: undefined = undefined;
+// null
+let pastReport: null = null;
+
+// not assignable to type
+let d: number;
+d = 12;
+
+// Array
+let mySubject: string[] = ["Math", "Programming"];
+let myNextNumber: number[] = [10, 12];
+
+// tuple
+let coordinates: [number, number] = [1, 3];
+// coordinates.push("d"); //assignable to parameter
