77e6e8ee107a241855fd6bcc123af84aa4894922
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-02 09:17:07 +0600

    omit required partial readonly

--- practice-m2/m2/src/2.11.ts
+++ practice-m2/m2/src/2.11.ts
@@ -1,11 +1,32 @@
 {
   // utility types
-  // pick
+
   type Person = {
     name: string;
     age: number;
     email?: string;
     contactNo: string;
   };
+  // Pick
   type NameAge = Pick<Person, "name" | "age">;
+
+  // Omit
+  type ContactInfo = Omit<Person, "name" | "age">;
+
+  // Required
+  type PersonRequired = Required<Person>;
+
+  // Partial
+  type PersonPartial = Partial<Person>;
+
+  // Readonly
+  type PersonReadonly = Readonly<Person>;
+  const person1: PersonReadonly = {
+    name: "Mr. XY",
+    age: 200,
+    contactNo: "0171234234",
+  };
+  // person1.name = "Mr. DC";
+
+  //
 }
