d816d41cd2b90248513eeddf186b62171aad4391
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-02 07:12:53 +0600

    asynchronous promise simulate module2 practice

---
+++ practice-m2/m2/src/2.8.ts
@@ -0,0 +1,24 @@
+{
+  // asynchronous promise
+
+  // simulate
+  const createPromise = (): Promise<string> => {
+    return new Promise<string>((resolve, reject) => {
+      const data: string = "something";
+      if (data) {
+        resolve(data);
+      } else {
+        reject("failed to load data");
+      }
+    });
+  };
+
+  // calling create promise function
+  const showData = async () => {
+    const data = await createPromise();
+    console.log(data);
+  };
+  showData();
+
+  //
+}
