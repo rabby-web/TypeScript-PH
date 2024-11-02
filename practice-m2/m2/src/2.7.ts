366f09b647d102151b7a37dffa62b182ccef000c
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-02 07:00:11 +0600

    generic constraint with keyof operator

--- practice-m2/m2/src/2.7.ts
+++ practice-m2/m2/src/2.7.ts
@@ -1,7 +1,30 @@
 {
-    // generic constraint with keyof operator
-    type Vehicle = {
-        
-    }
-    // 
+  // generic constraint with keyof operator
+  type Vehicle = {
+    bike: string;
+    car: string;
+    ship: string;
+  };
+  type Owner = "bike" | "car" | "ship"; // manually
+  type Owner2 = keyof Vehicle; // keyof operator
+
+  const getPropertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+    return obj[key];
+  };
+
+  const user = {
+    name: "Mr. XX",
+    age: 25,
+    address: "ctg",
+  };
+  const result1 = getPropertyValue(user, "name");
+
+  //   const user = {
+  //     name: "Mr. XX",
+  //     age: 25,
+  //     address: "ctg",
+  //   };
+  //   user['age']
+
+  //
 }
