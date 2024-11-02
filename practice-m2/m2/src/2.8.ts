a2eb3d593a419cd4bd8834b1fb5cf95f92eae0fa
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-02 07:35:45 +0600

    asynchronous promise data load data

--- practice-m2/m2/src/2.8.ts
+++ practice-m2/m2/src/2.8.ts
@@ -1,10 +1,27 @@
 {
   // asynchronous promise
+  type Todo = {
+    id: number;
+    userId: number;
+    title: string;
+    completed: boolean;
+  };
+
+  const getTodo = async (): Promise<Todo> => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const data = await response.json();
+    return data;
+    // console.log(data);
+  };
+  getTodo();
+  type Something = { something: string };
 
   // simulate
-  const createPromise = (): Promise<string> => {
-    return new Promise<string>((resolve, reject) => {
-      const data: string = "something";
+  const createPromise = (): Promise<Something> => {
+    return new Promise<Something>((resolve, reject) => {
+      const data: Something = { something: "something" };
       if (data) {
         resolve(data);
       } else {
@@ -14,9 +31,10 @@
   };
 
   // calling create promise function
-  const showData = async () => {
-    const data = await createPromise();
-    console.log(data);
+  const showData = async (): Promise<Something> => {
+    const data: Something = await createPromise();
+    return data;
+    // console.log(data);
   };
   showData();
 
