b5da4061ea4b2056735cb0c2c74c919f459a90c6
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-04 21:12:04 +0600

    type guard practice

---
+++ practice-m3/m3/src/3.3.ts
@@ -0,0 +1,43 @@
+{
+    // type guard
+    // type of
+  
+    type Alphanumeric = string | number;
+  
+    const add = (param1: Alphanumeric, param2: Alphanumeric): Alphanumeric => {
+      if (typeof param1 === "number" && typeof param2 === "number") {
+        return param1 + param2;
+      } else {
+        return param1.toString() + param2.toString();
+      }
+    };
+  
+    const result1 = add(2, 3);
+    console.log(result1);
+  
+    // in guard
+    type NormalUser = {
+      name: string;
+    };
+    type AdminUser = {
+      name: string;
+      role: "admin";
+    };
+    const getUser = (user: NormalUser | AdminUser) => {
+      if ("role" in user) {
+        console.log(`My name is ${user.name} and my role is ${user.role}`);
+      } else {
+        console.log(`My name is ${user.name}`);
+      }
+    };
+    const normalUser: NormalUser = {
+      name: "Mr. Normal",
+    };
+    const adminUser: AdminUser = {
+      name: "Mr. Admin",
+      role: "admin",
+    };
+    getUser(normalUser);
+  
+    //
+  }
