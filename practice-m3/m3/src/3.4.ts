be65b01c7411700e6b4f6a9fbb1d987e7ded5308
rabby-web <rabby.webdeveloper@gmail.com>
2024-11-04 21:15:47 +0600

    instance of practice

---
+++ practice-m3/m3/src/3.4.ts
@@ -0,0 +1,54 @@
+{
+    // instance of
+    class Animal {
+      name: string;
+      species: string;
+      constructor(name: string, species: string) {
+        this.name = name;
+        this.species = species;
+      }
+      makeSound() {
+        console.log("I am making sound");
+      }
+    }
+    class Dog extends Animal {
+      constructor(name: string, species: string) {
+        super(name, species);
+      }
+      makeBark() {
+        console.log("I am barking");
+      }
+    }
+    class Cat extends Animal {
+      constructor(name: string, species: string) {
+        super(name, species);
+      }
+      makeMew() {
+        console.log("I am mewing");
+      }
+    }
+    //   smart way handle function
+    const isDog = (animal: Animal): animal is Dog => {
+      return animal instanceof Dog;
+    };
+    const isCat = (animal: Animal): animal is Cat => {
+      return animal instanceof Cat;
+    };
+  
+    const getAnimal = (animal: Animal) => {
+      if (isDog(animal)) {
+        animal.makeBark();
+      } else if (isCat(animal)) {
+        animal.makeMew();
+      } else {
+        animal.makeSound();
+      }
+    };
+  
+    const dog = new Dog("Dog", "dog");
+    const cat = new Cat("Dog", "dog");
+  
+    getAnimal(dog);
+  
+    //
+  }
